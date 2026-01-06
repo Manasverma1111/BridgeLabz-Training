25abf219453a6bc72d06aa959cb8e3cdedfb7be4
Manasverma1111 <vmanas79@gmail.com>
2026-01-06 15:33:26 +0530

    [Manas Verma] add . added LeetCode Ques

---
+++ oops-practice/leet-code-codebase/RemoveElements.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class RemoveElements {
+
+    public static int removeElement(int[] nums, int val) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        return idx;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter value to remove: ");
+        int val = sc.nextInt();
+        int k = removeElement(nums, val);
+
+        System.out.print("Array after removing element: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        System.out.println("\nNumber of remaining elements: " + k);
+
+    }
+}
