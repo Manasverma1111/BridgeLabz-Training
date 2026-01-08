321604629d7c498e7b8fe9bc99d633aeb5106e27
Manasverma1111 <vmanas79@gmail.com>
2026-01-08 22:11:16 +0530

    [Manas Verma] add . added LeetCode Ques

--- oops-practice/leet-code-codebase/ReverseStringIII.java
+++ oops-practice/leet-code-codebase/ReverseStringIII.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class ReverseStringIII {
+import java.util.Scanner;
+
+class ReverseStringIII {
+
+    public String reverseWords(String s) {
+        String[] str = s.split(" ");
+        StringBuilder resStr = new StringBuilder();
+
+        for (String st : str) {
+            resStr.append(new StringBuilder(st).reverse()).append(" ");
+        }
+
+        return resStr.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+        ReverseStringIII res = new ReverseStringIII();
+        String result = res.reverseWords(str);
+
+        System.out.println("Reversed string: " + result);
+
+    }
 }
